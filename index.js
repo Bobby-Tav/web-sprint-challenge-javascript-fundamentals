7308a1f7bb1c3f7902da5ab178cb0b2edb0a2e46
Robert Taveras <monkeyeden@Roberts-MBP.lan>
2021-04-23 14:26:45 -0400

    did the stretch in topic 2

--- index.js
+++ index.js
@@ -123,29 +123,29 @@ const zooAnimals = [
     * The consume function should return the invocation of cb, passing a and b into cb as arguments
   */
 
-  function consume(/*Your Code Here */){
-    /*Your Code Here */
-  }
+  // function consume(a,b,cb){
+  //   return cb(a,b);
+  // }
  
   
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
  // 🦁🦁🦁 Use add to return the sum of two numbers 🦁🦁🦁
   
-function add(/*Your Code Here */){
-    /*Your Code Here*/
-  }
+// function add(a,b){
+//     return a + b; 
+//   }
 
 // 🦁🦁🦁 Use multiply to return the product of two numbers 🦁🦁🦁
   
-function multiply(/*Your Code Here */){
-   /*Your Code Here */
-  }
+// function multiply(a , b){
+//    return a * b;
+//   }
 
  // 🦁🦁🦁 Use greeting to accept a first and last name and return "Hello {first-name} {last-name}, nice to meet you!" 🦁🦁🦁
   
-function greeting(/*Your Code Here */){
-   return /*Your Code Here */
-  }
+// function greeting(a,b){
+//    return `Hello ${a} ${b}, nice to meet you!`;
+//   }
   
   // 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 
   // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
@@ -155,7 +155,46 @@ function greeting(/*Your Code Here */){
   
   
 // 🦁💪 Stretch: If you haven't already, convert your array method callbacks into arrow functions - make sure you comment out this section before you submit your work 🦁💪
+
+// function animalNames(animalArray){
+//   let nameArray = [];
+//   animalArray.forEach(item => nameArray.push(`name: ${item.animal_name}, scientific: ${item.scientific_name}`))
+//   return nameArray;
+// }
+
+// function lowerCaseNames(animalArray){ 
+//   const newArray = animalArray.map(item => item.animal_name.toLowerCase());
+//   return newArray;
+// }
+
+// function lowPopulationAnimals(animalArray){
+//   const lowPop = animalArray.filter(item => item.population <5);
+//   return lowPop;
+// }
+
+// function USApop(animalArray){
+//   const totalPop = animalArray.reduce((acc,item) =>{
+//     return acc + item.population;
+//   },0);
+//   return totalPop;
+// }
+
+const consume = (a,b,cb) => cb(a,b);
+
+const add = (a,b) => a + b;
+
+const multiply = (a , b) => a * b;
+
+const greeting = (a,b) => `Hello ${a} ${b}, nice to meet you!`;
  
+
+
+// console.log(consume(2, 2, add)); // 4
+  // console.log(consume(10, 16, multiply)); // 160
+  console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+  
+ 
+
 
 
 
